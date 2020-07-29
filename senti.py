f0c94e4a37ee65b14f3a89e2a3ab84e33ddb05f3
Devansh Dixit <64734319+DevanshD3@users.noreply.github.com>
2020-07-30 00:24:10 +0530

    Update senti.py

--- senti.py
+++ senti.py
@@ -34,15 +34,4 @@ for e in entities:
     print(e.name,  e.metadata, e.salience)
 
 
-# document = language.types.Document(
-#      content=text,
-#      type=language.enums.Document.Type.PLAIN_TEXT,
-# )
-#  response = client.analyze_entities(
-#     document=document,
-#      encoding_type='UTF32',
-# )
-# >> > for entity in response.entities:
-# ... print('=' * 20)
-# ... print('         name: {0}'.format(entity.name))
-# ... print('         type: {0}'.format(entity.type)
+
