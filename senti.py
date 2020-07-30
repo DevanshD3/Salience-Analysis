38ea74d031569e648351da912e91432329991aba
Rohit Rajesh <rajesh.rohit04@gmail.com>
2020-07-30 13:44:21 +0530

    Updated with better results

---
+++ senti.py
@@ -0,0 +1,35 @@
+import sys
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"sentiment-analysis-284609-a234a6af795c.json"
+def language_analysis(text):
+    client = language.LanguageServiceClient()
+    document = types.Document(
+        content=text,
+        type=enums.Document.Type.PLAIN_TEXT)
+    sentiment = client.analyze_sentiment(document=document).document_sentiment
+    ent_analysis = client.analyze_entities(
+        document=document, encoding_type='UTF32')
+    dir(ent_analysis)
+    entities = ent_analysis.entities
+    return sentiment, entities
+# text = sys.argv[1]
+text = 'Artificial neural networks, usually simply called neural networks, or connectionist systems are computing systems vaguely inspired by the biological neural networks that constitute animal brains. The data structures and functionality of neural nets are designed to simulate associative memory.'
+items = []
+sentiment, entities = language_analysis(text)
+
+dicti = {}
+for e in entities:
+    if e.name not in items:
+        items.append(e.name) 
+        dicti[e.name] = 0
+
+for i in items:
+    for e in entities:
+        if e.name == i:
+            dicti[i] += float(e.salience)
+
+for key_val in dicti.keys():
+    print(f'{key_val}: {dicti[key_val]}')
