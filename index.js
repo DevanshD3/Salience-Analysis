e57ace4534193ce2b002521d4744337e3898144e
Manthan <manthan.asher@gmail.com>
2020-08-10 00:46:54 +0530

    some changes

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ app.get("/result", (req, res) => {
   });
 });
 
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
