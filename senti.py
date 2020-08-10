5a616ee09283fbb683294ebabb1cd2fa4af28f91
Devansh Dixit <64734319+DevanshD3@users.noreply.github.com>
2020-08-10 16:35:26 +0530

    Update senti.py

--- senti.py
+++ senti.py
@@ -26,6 +26,6 @@ sentiment, entities = language_analysis(text)
 
 dicti = {}
 for e in entities:
-    print(f'Word: {e.name}\nType: {e.type}\nSalience: {round(e.salience)}\n')
+    print(f'Word: {e.name}\nType: {e.type}\nSalience: {round(e.salience,2)}\n')
 
 print(f'Sentiment\n{sentiment}')
